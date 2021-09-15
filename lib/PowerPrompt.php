c53c31197d3778c61af3df82d8795244ffd8d55d
Jacob Neergaard Pedersen <neergaard@kananga.dk>
2021-09-15 13:24:48 +0200

    change to singleton

--- lib/PowerPrompt.php
+++ lib/PowerPrompt.php
@@ -1,145 +1,137 @@
 <?php
 class PowerPrompt {
-	private static $width = null;
-	private static $height = null;
-	private static $stdin = null;
-	
-	private static $option = [];
-	private static $input = '';
-	
-	static public function header($text) {
-		self::init();
-		self::setpos(1,1);
-		
+	private static $instance = null;
+
+	private $width = null;
+	private $height = null;
+	private $stdin = null;
+
+	private $option = [];
+	private $input = '';
+
+	private function __construct() {
+		if($this->stdin===null) {
+			$this->width = exec('tput cols');
+			$this->height = exec('tput lines');
+
+			system('stty cbreak');
+			$this->clear_screen();
+			$this->stdin = fopen('php://stdin', 'r');
+		}
+	}
+
+	public function header($text) {
+		$this->setpos(1,1);
+
 		$len = mb_strlen($text)+2;
-		
-		self::style('white');
-		echo mb_str_pad('',ceil((self::$width-$len)/2),'=');
-		self::style('blue','bold');
+
+		$this->style('white');
+		echo str_pad('',ceil(($this->width-$len)/2),'=');
+		$this->style('blue','bold');
 		echo ' '.$text.' ';
-		self::style('white','normal');
-		echo mb_str_pad('',floor((self::$width-$len)/2),'=');
-		self::style();
+		$this->style('white','normal');
+		echo str_pad('',floor(($this->width-$len)/2),'=');
+		$this->style();
 		echo PHP_EOL;
 	}
-	static public function reset_option() {
-		self::$option = [];
+	public function reset_option() {
+		$this->option = [];
 	}
-	static public function add_option($command,$title,$function,$type = null) {
-		self::init();
-		
-		self::$option[] = [
+	public function add_option($command,$title,$function,$type = null) {
+		$this->option[] = [
 			'command' => $command,
 			'function' => $function,
 		];
-		
-		self::setpos(3+sizeof(self::$option),1);
-		
-		self::style('bold');
-		echo mb_str_pad($command.':',5,' ');
-		self::style();
+
+		$this->setpos(3+sizeof($this->option),1);
+
+		$this->style('bold');
+		echo str_pad($command.':',5,' ');
+		$this->style();
 		echo $title.PHP_EOL;
 	}
-	static public function select_option($title,$preset = '') {
-		self::init();
-		
-		self::$input = $preset;
-		
+	public function select_option($title,$preset = '') {
+		$this->input = $preset;
+
 		while(true) {
-			self::setpos(2,1);
+			$this->setpos(2,1);
 			echo $title.': ';
-			self::style('red','bold');
-			echo self::$input;
-			self::style();
-			self::clearline();
-			$key = fgetc(self::$stdin);
-			
-			self::setpos(10,1);
-			
+			$this->style('red','bold');
+			echo $this->input;
+			$this->style();
+			$this->clearline();
+			$key = fgetc($this->stdin);
+
+			$this->setpos(10,1);
+
 			if($key==chr(10)) { // Enter
-				echo "ENTER";
-				self::execute_option(self::$input);
+				$this->execute_option($this->input);
 			}
 			elseif($key==chr(4)) { // EOT
-				self::exit();
+				$this->exit();
 			}
 			elseif($key==chr(127) || $key==chr(8)) { // Backspace
-				self::$input = mb_substr(self::$input,0,-1);
+				$this->input = mb_substr($this->input,0,-1);
 			}
 			elseif($key==chr(9)) { // Tab
 			}
 			elseif($key==chr(27)) { // Escape
-				$subkey = self::fgetc_purge();
+				$subkey = $this->fgetc_purge();
 				switch($subkey) {
-					case '';
-						//echo "ESC";
-						self::execute_option("ESC");
+					case ''; // ESC Key
+						$this->execute_option('ESC');
 						break;
 					case chr(91).chr(65): // Arrow Up
-						echo "UP";
 						break;
 					case chr(91).chr(66): // Arrow Down
-						echo "DOWN";
 						break;
 					default:
-						echo bin2hex($subkey);
+						echo 'Unknown:'.bin2hex($subkey);
 				}
 			}
 			else {
-				//echo bin2hex($key).': ';
-				self::$input .= $key;
+				$this->input .= $key;
 			}
 		}
 	}
-	static public function clear_screen() {
+	public function clear_screen() {
 		echo chr(27).'[2H'.chr(27).'[J';
 	}
-	static private function execute_option($input) {
+	private function execute_option($input) {
 		echo $input;
-		foreach(self::$option as $var) {
+		foreach($this->option as $var) {
 			if($var['command']===$input) {
 				$func = $var['function'];
-				$func();
+				$func(self::$instance);
 			}
 		}
 	}
-	static private function fgetc_purge() {
+	private function fgetc_purge() {
 		$return = '';
-		
+
 		while(true) {
-			$read = [self::$stdin];
+			$read = [$this->stdin];
 			$write = $except = [];
 			if(stream_select($read, $write, $except, 0, 1)) {
-				$return .= fgetc(self::$stdin);
+				$return .= fgetc($this->stdin);
 			}
 			else {
 				return $return;
 			}
 		}
 	}
-	static private function init() {
-		if(self::$stdin===null) {
-			self::$width = exec('tput cols');
-			self::$height = exec('tput lines');
-			
-			system('stty cbreak');
-			self::clear_screen();
-			self::$stdin = fopen('php://stdin', 'r');
-			stream_set_timeout(self::$stdin, 2);
-		}
-	}
-	static private function exit() {
-		self::clear_screen();
-		echo "Bye".PHP_EOL;
+	private function exit() {
+		$this->clear_screen();
+		echo 'Bye'.PHP_EOL;
 		exit;
 	}
-	static private function setpos($i,$j) {
+	private function setpos($i,$j) {
 		echo chr(27).'['.$i.';'.$j.'H';
 	}
-	static private function clearline() {
+	private function clearline() {
 		echo chr(27).'[K';
 	}
-	static public function style(...$styles) {
+	public function style(...$styles) {
 		static $_styles = [
 			'red'				=> "[31m",
 			'green'			=> "[32m",
@@ -151,7 +143,7 @@ class PowerPrompt {
 			'normal'		=> '[22m',
 			'bold'			=> '[1m',
 		];
-		
+
 		if($styles) {
 			foreach($styles as $style) {
 				if(isset($_styles[$style])) {
@@ -162,5 +154,11 @@ class PowerPrompt {
 		else {
 			echo chr(27).'[0m';
 		}
+	}
+	public static function getInstance() {
+		if(self::$instance == null) {
+			self::$instance = new PowerPrompt();
+		}
+		return self::$instance;
 	}
 }
